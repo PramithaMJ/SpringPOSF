f3b8ee83dfaaa026777463c6055f7b6298ea37f5
PramithaMJ <lpramithamj@gmail.com>
2023-06-27 12:46:25 +0530

    create pojo

--- src/main/java/com/springbootacademy/springbatch10pos/entity/Customer.java
+++ src/main/java/com/springbootacademy/springbatch10pos/entity/Customer.java
@@ -26,4 +26,77 @@ public class Customer {
 
     @Column(name = "active_state",columnDefinition = "TINYINT default 0 ")
     private boolean activeStatus;
+
+    public Customer() {
+    }
+
+    public Customer(int customerId, String customerName, String customerAddress, double customerSalery, String nic, boolean activeStatus) {
+        this.customerId = customerId;
+        this.customerName = customerName;
+        this.customerAddress = customerAddress;
+        this.customerSalery = customerSalery;
+        this.nic = nic;
+        this.activeStatus = activeStatus;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerAddress() {
+        return customerAddress;
+    }
+
+    public void setCustomerAddress(String customerAddress) {
+        this.customerAddress = customerAddress;
+    }
+
+    public double getCustomerSalery() {
+        return customerSalery;
+    }
+
+    public void setCustomerSalery(double customerSalery) {
+        this.customerSalery = customerSalery;
+    }
+
+    public String getNic() {
+        return nic;
+    }
+
+    public void setNic(String nic) {
+        this.nic = nic;
+    }
+
+    public boolean isActiveStatus() {
+        return activeStatus;
+    }
+
+    public void setActiveStatus(boolean activeStatus) {
+        this.activeStatus = activeStatus;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "customerId=" + customerId +
+                ", customerName='" + customerName + '\'' +
+                ", customerAddress='" + customerAddress + '\'' +
+                ", customerSalery=" + customerSalery +
+                ", nic='" + nic + '\'' +
+                ", activeStatus=" + activeStatus +
+                '}';
+    }
+    
 }
