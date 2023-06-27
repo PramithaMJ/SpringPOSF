686ae7dceb5ea61c96e1df8055f2ef354eded0fe
PramithaMJ <lpramithamj@gmail.com>
2023-06-27 11:17:28 +0530

    add application property

--- src/main/java/com/springbootacademy/springbatch10pos/Springbatch10posApplication.java
+++ src/main/java/com/springbootacademy/springbatch10pos/Springbatch10posApplication.java
@@ -3,7 +3,7 @@ package com.springbootacademy.springbatch10pos;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication //Define Spring boot project
 public class Springbatch10posApplication {
 
 	public static void main(String[] args) {
