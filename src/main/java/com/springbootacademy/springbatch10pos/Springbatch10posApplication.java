03ec8e26935117e9d2ebb7badde5554d469377d9
PramithaMJ <lpramithamj@gmail.com>
2023-06-27 10:41:06 +0530

    first commit

---
+++ src/main/java/com/springbootacademy/springbatch10pos/Springbatch10posApplication.java
@@ -0,0 +1,13 @@
+package com.springbootacademy.springbatch10pos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springbatch10posApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springbatch10posApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springbootacademy/springbatch10pos/Springbatch10posApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootacademy.springbatch10pos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springbatch10posApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
