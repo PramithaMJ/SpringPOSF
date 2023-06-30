cd6b456dfa55bb01e2068d1575f7a341c73dc323
PramithaMJ <lpramithamj@gmail.com>
2023-06-30 23:19:40 +0530

    Post mapping

--- src/main/java/com/springbootacademy/springbatch10pos/controller/CustomerController.java
+++ src/main/java/com/springbootacademy/springbatch10pos/controller/CustomerController.java
@@ -1,6 +1,7 @@
 package com.springbootacademy.springbatch10pos.controller;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin // get request of all localhost
 public class CustomerController {
 
+    @PostMapping
     public String saveCustomer() {
         return "saved";
     }
