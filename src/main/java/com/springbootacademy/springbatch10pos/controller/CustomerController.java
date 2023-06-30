c74cc4392d463d81468bcd298f4ca79ee1541311
PramithaMJ <lpramithamj@gmail.com>
2023-06-30 23:16:12 +0530

    requesr

--- src/main/java/com/springbootacademy/springbatch10pos/controller/CustomerController.java
+++ src/main/java/com/springbootacademy/springbatch10pos/controller/CustomerController.java
@@ -1,9 +1,11 @@
 package com.springbootacademy.springbatch10pos.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/v1/customer")
+@CrossOrigin
 public class CustomerController {
 }
