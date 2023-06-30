e8464c6a2d4f1a8120456f1b04791f64a9e55ce2
PramithaMJ <lpramithamj@gmail.com>
2023-07-01 00:36:37 +0530

    service

--- src/main/java/com/springbootacademy/springbatch10pos/service/impl/CustomerServiceIMPL.java
+++ src/main/java/com/springbootacademy/springbatch10pos/service/impl/CustomerServiceIMPL.java
@@ -2,9 +2,11 @@ package com.springbootacademy.springbatch10pos.service.impl;
 
 import com.springbootacademy.springbatch10pos.dto.CustomerDTO;
 import com.springbootacademy.springbatch10pos.service.CustomerService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class CustomerServiceIMPL implements CustomerService {
-    
+
     @Override
     public String saveCustomer(CustomerDTO customerDTO) {
         return null;
