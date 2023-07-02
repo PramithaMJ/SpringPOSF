06452888010a395e8c3df4f025e97d6fdd3b177f
PramithaMJ <lpramithamj@gmail.com>
2023-07-02 21:32:29 +0530

    repo data tranfer

--- src/main/java/com/springbootacademy/springbatch10pos/service/impl/CustomerServiceIMPL.java
+++ src/main/java/com/springbootacademy/springbatch10pos/service/impl/CustomerServiceIMPL.java
@@ -1,15 +1,30 @@
 package com.springbootacademy.springbatch10pos.service.impl;
 
 import com.springbootacademy.springbatch10pos.dto.CustomerDTO;
+import com.springbootacademy.springbatch10pos.entity.Customer;
+import com.springbootacademy.springbatch10pos.repo.CustomerRepo;
 import com.springbootacademy.springbatch10pos.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CustomerServiceIMPL implements CustomerService {
 
- @Override
+    @Autowired
+    private CustomerRepo customerRepo;
+
+
+    @Override
     public String saveCustomer(CustomerDTO customerDTO) {
-        System.out.println("service customer"+customerDTO);
-        return null;
+        Customer customer = new Customer(
+                customerDTO.getCustomerId(),
+                customerDTO.getCustomerName(),
+                customerDTO.getCustomerAddress(),
+                customerDTO.getCustomerSalery(),
+                customerDTO.getNic(),
+                customerDTO.isActiveStatus()
+        );
+        customerRepo.save(customer);
+        return "saved";
     }
 }
